1be7543039e7def105867c1bb107f4bfb12f6ace
ShivamKumarJha <jha.shivam3@gmail.com>
2025-02-10 18:46:28 +0000

    Add drivers/gpu/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -1064,7 +1064,11 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 	ret = sysfs_create_link(&device->dev->kobj,
 			&devfreq->dev.kobj, "devfreq");
 
-	pwrscale->devfreq_wq = create_freezable_workqueue("kgsl_devfreq_wq");
+#if VENDOR_EDIT
+    pwrscale->devfreq_wq = alloc_workqueue("%s", __WQ_LEGACY | WQ_FREEZABLE | WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_UX, 1, "kgsl_devfreq_wq");
+#else
+    pwrscale->devfreq_wq = create_freezable_workqueue("kgsl_devfreq_wq");
+#endif
 	INIT_WORK(&pwrscale->devfreq_suspend_ws, do_devfreq_suspend);
 	INIT_WORK(&pwrscale->devfreq_resume_ws, do_devfreq_resume);
 	INIT_WORK(&pwrscale->devfreq_notify_ws, do_devfreq_notify);
